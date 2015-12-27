da9880d62bdc6e07671262dee95f5b3ab1a7dfd0
Jonathan Gingras <jonathan.gingras.1@gmail.com>
2015-12-27 13:50:21 -0500

    cleaning

--- base64.c
+++ base64.c
@@ -2,17 +2,6 @@
 #include <stdlib.h>
 #include "base64.h"
 
-#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
-#define BYTETOBINARY(byte)  \
-  ((byte) & 0x80 ? 1 : 0), \
-  ((byte) & 0x40 ? 1 : 0), \
-  ((byte) & 0x20 ? 1 : 0), \
-  ((byte) & 0x10 ? 1 : 0), \
-  ((byte) & 0x08 ? 1 : 0), \
-  ((byte) & 0x04 ? 1 : 0), \
-  ((byte) & 0x02 ? 1 : 0), \
-  ((byte) & 0x01 ? 1 : 0)
-
 static char base64_encode_table[] = {
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
 	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
